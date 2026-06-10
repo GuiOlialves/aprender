4190e1a2ab29144b678517dd8b327bff84c006a5
GuiOlialves <guilhermeolialves37@gmail.com>
2026-06-10 19:09:02 -0300

    projeto analise de vendas

--- aula2/analiseVendas.py
+++ aula2/analiseVendas.py
@@ -14,10 +14,11 @@ clientes = pd.DataFrame({
 })
 
 vendas = pd.DataFrame({
-    "id_cliente": [1, 2, 2, 3, 5, 5, 5, None],
+    "id": [1, 2, 2, 3, 5, 5, 5, None],
     "produto": ["notebook", "mouse", "teclado", "monitor", "mouse", "notebook", "teclado", "mouse"],
     "valor": [3500, 150, 280, 1200, 150, 3500, 280, None]
 })
+
 
 print("bem vindo ao nosso sistema de analise de vendads!")
 print("-------------------------------------------------")
@@ -28,4 +29,26 @@ print("3 - Calcular o total gasto por cliente")
 print("4 - Calcular o total gasto por cidade")
 print("5 - Descobrir qual produto vendeu mais")
 print("6 - Filtrar só as vendas acima de R$200")
+print("7 - Sair")
+resp1 = int(input(""))
+bd = pd.merge(clientes,vendas, on="id")
+if resp1 == 1:
+    vendas["id"] = vendas["id"].fillna(0)    
+    vendas["valor"] = vendas["valor"].fillna(0)
+if resp1 == 2:
+    print(pd.merge(clientes,vendas, on="id"))
+if resp1 == 3:  
+    print(bd.groupby("nome")["valor"].sum())
+if resp1 == 4:
+    print(bd.groupby("cidade")["valor"].sum())
+if resp1 == 5:
+    contagem = bd.groupby("produto")["valor"].count()
+    maisVendido = contagem.idxmax()
+    print(f"Produto mais vendido: {maisVendido} ({contagem[maisVendido]}) vendas")    
+if resp1 == 6:
+    print(bd[bd["valor"] >= 200])
+
+
+
+
 
