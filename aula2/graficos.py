4bf9da7744fdf870d25004c69461cdc48c059aa8
GuiOlialves <guilhermeolialves37@gmail.com>
2026-06-11 22:57:31 -0300

    atualizações graficos 2

--- aula2/graficos.py
+++ aula2/graficos.py
@@ -1,11 +1,12 @@
 import seaborn as sns
 import pandas as pd
-
+import matplotlib.pyplot as plt
 df = sns.load_dataset("tips")
 # dataset clássico de gorjetas em restaurante
 # colunas: total_bill, tip, sex, smoker, day, time, size
 print(df.head())
-
-
+# sns.scatterplot(data=df, x="total_bill", y="tip", hue="sex")
+# plt.title("Gorjeta vs Conta Total")
+# plt.show()
 
 
